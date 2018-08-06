47dbce8348e8f1135192107e4cb2a1c116d0fca8
audreybernstein <audrey.bernstein@aol.com>
2018-08-06 17:22:43 +0000

    automatically backed up by learn

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,5 @@
 ls;
-mkdor this_is_a_directory_made_in_a_script;
+mkdir this_is_a_directory_made_in_a_script;
 mkdir another_directory;
 touch this_is_a_directory_made_in_a_script/first_file.txt;
 touch this_is_a_directory_made_in_a_script/second_file.txt;
